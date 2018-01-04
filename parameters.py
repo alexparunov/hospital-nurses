90959e57c30039331326eca71e3fd6b49a78fa1e
Alexander Parunov <alexander.parunov@gmail.com>
2018-01-04 17:25:32 +0400

    big boys started

--- parameters.py
+++ parameters.py
@@ -1,3 +1,5 @@
+params = []
+
 param1 = {
 	'nNurses':17,
 	'nHours':24,
@@ -7,7 +9,7 @@ param1 = {
 	'maxPresence':24,
 	'demand':[10, 5, 5, 11, 4, 2, 6, 2, 11, 5, 10, 6, 11, 6, 6, 1, 5, 10, 6, 1, 8, 10, 9, 3]
 }
-
+params.append(param1)
 
 param2 = {
 	"nNurses":18,
@@ -18,6 +20,7 @@ param2 = {
 	"maxPresence":19,
 	"demand":[7, 9, 12, 11, 8, 1, 3, 5, 6, 9, 12, 7, 2, 11, 2, 3, 12, 8, 9, 4, 2, 6, 2, 5];
 }
+params.append(param2)
 
 param3 = {
 	"nNurses": 20,
@@ -28,7 +31,7 @@ param3 = {
 	"maxPresence": 14,
 	"demand": [2, 2, 1, 1, 1, 2, 2, 3, 4, 6, 6, 7, 5, 8, 8, 7, 6, 6, 4, 3, 4, 3, 3, 3]
 }
-
+params.append(param3)
 
 param4 = {
 	'nNurses':30,
@@ -39,6 +42,7 @@ param4 = {
 	'maxPresence':24,
 	'demand':[13, 2, 21, 3, 6, 17, 5, 9, 9, 3, 8, 8, 1, 5, 18, 14, 17, 15, 5, 10, 18, 18, 8, 2]
 }
+params.append(param4)
 
 param5 = {
 	'nNurses':48,
@@ -49,6 +53,7 @@ param5 = {
 	'maxPresence':21,
 	'demand':[11, 15, 21, 32, 3, 14, 21, 32, 27, 18, 14, 23, 8, 15, 29, 10, 3, 3, 17, 27, 2, 9, 10, 26]
 }
+params.append(param5)
 
 param6 = {
 	'nNurses':66,
@@ -59,6 +64,7 @@ param6 = {
 	'maxPresence':22,
 	'demand':[46, 15, 19, 21, 2, 27, 35, 41, 15, 5, 27, 41, 27, 1, 33, 31, 25, 37, 10, 3, 1, 38, 2, 16]
 }
+params.append(param6)
 
 param7 = {
 	'nNurses':72,
@@ -69,7 +75,117 @@ param7 = {
 	'maxPresence':24,
 	'demand':[35, 25, 41, 33, 24, 36, 23, 38, 48, 22, 47, 5, 31, 23, 45, 43, 8, 37, 46, 44, 42, 45, 44, 23]
 }
+params.append(param7)
 
+param8 = {
+	'nNurses':80,
+	'nHours':24,
+	'minHours':1,
+	'maxHours':18,
+	'maxConsec':17,
+	'maxPresence':24,
+	'demand':[3, 25, 41, 29, 47, 37, 12, 27, 35, 45, 16, 3, 38, 49, 28, 10, 47, 22, 27, 25, 41, 34, 46, 55]
+}
+params.append(param8)
+
+param9 = {
+	'nNurses':92,
+	'nHours':24,
+	'minHours':9,
+	'maxHours':24,
+	'maxConsec':10,
+	'maxPresence':24,
+	'demand':[55, 34, 29, 43, 20, 31, 1, 5, 21, 11, 64, 24, 15, 11, 27, 59, 8, 35, 36, 43, 53, 24, 18, 8]
+}
+params.append(param9)
+
+param10 = {
+	'nNurses':110,
+	'nHours':24,
+	'minHours':8,
+	'maxHours':13,
+	'maxConsec':11,
+	'maxPresence':24,
+	'demand':[12, 25, 16, 50, 1, 68, 28, 4, 76, 69, 35, 62, 17, 6, 75, 29, 1, 17, 19, 11, 41, 61, 37, 31]
+}
+params.append(param10)
+
+param11 = {
+	'nNurses':115,
+	'nHours':24,
+	'minHours':2,
+	'maxHours':14,
+	'maxConsec':2,
+	'maxPresence':24,
+	'demand':[20, 6, 78, 64, 70, 59, 34, 22, 43, 23, 53, 3, 42, 15, 5, 39, 80, 68, 1, 79, 34, 73, 52, 2]
+}
+params.append(param11)
+
+param12 = {
+	'nNurses':138,
+	'nHours':24,
+	'minHours':3,
+	'maxHours':23,
+	'maxConsec':19,
+	'maxPresence':24,
+	'demand':[36, 61, 24, 65, 96, 37, 18, 81, 95, 18, 33, 86, 37, 14, 59, 53, 63, 67, 35, 38, 93, 81, 57, 19]
+}
+params.append(param12)
+
+param13 = {
+	'nNurses':144,
+	'nHours':24,
+	'minHours':2,
+	'maxHours':20,
+	'maxConsec':9,
+	'maxPresence':24,
+	'demand':[8, 59, 72, 89, 100, 89, 2, 37, 3, 32, 36, 52, 5, 16, 69, 38, 3, 67, 93, 68, 12, 62, 10, 11]
+}
+params.append(param13)
+
+param14 = {
+	'nNurses':168,
+	'nHours':24,
+	'minHours':10,
+	'maxHours':18,
+	'maxConsec':2,
+	'maxPresence':24,
+	'demand':[26, 23, 65, 20, 46, 43, 101, 107, 1, 54, 95, 101, 94, 15, 86, 98, 27, 61, 58, 12, 106, 99, 47, 3]
+}
+params.append(param14)
+
+param15 = {
+	'nNurses':184,
+	'nHours':24,
+	'minHours':7,
+	'maxHours':17,
+	'maxConsec':7,
+	'maxPresence':24,
+	'demand':[76, 82, 122, 27, 46, 62, 1, 44, 75, 81, 97, 116, 14, 113, 63, 14, 25, 29, 57, 76, 85, 37, 9, 54]
+}
+params.append(param15)
+
+param16 = {
+	'nNurses':207,
+	'nHours':24,
+	'minHours':10,
+	'maxHours':19,
+	'maxConsec':5,
+	'maxPresence':24,
+	'demand':[60, 124, 65, 90, 110, 131, 95, 68, 39, 16, 51, 20, 128, 73, 72, 44, 22, 44, 17, 89, 131, 110, 73, 45]
+}
+params.append(param16)
+
+param17 = {
+	'nNurses':220,
+	'nHours':24,
+	'minHours':2,
+	'maxHours':18,
+	'maxConsec':17,
+	'maxPresence':24,
+	'demand':[75, 112, 122, 13, 79, 115, 38, 27, 11, 94, 54, 39, 126, 64, 93, 57, 119, 113, 127, 154, 116, 10, 151, 113]
+}
+params.append(param17)
 
 # This is used just for testings
 param_very_big = {
@@ -81,5 +197,4 @@ param_very_big = {
 	"maxPresence": 24,
 	"demand": [964, 650, 966, 1021, 824, 387, 828, 952, 611, 468, 403, 561, 862, 597, 1098, 855, 918, 1016, 897, 356, 615, 670, 826, 349]
 }
-
-params = [param0, param1, param_very_big]
+params.append(param_very_big)
--- plot_results.py
+++ plot_results.py
@@ -12,9 +12,9 @@ grasp_objective = [1379, 1640, 1726, 1492, 1467, 1631, 2107, 2268, 2714, 2675, 2
 
 # Those values and results are coming from parameters.py file named as param0,param1,...param{index_of_array}
 # ILP results
-number_instances = [17, 18, 20, 30, 48, 66, 72]
-ilp_objective =    [11, 18, 11, 21, 47, 62, 49]
-ilp_time =         [0.72, 0.8, 2.57, 1.17, 5.72, 3.21, 23.89]
+number_instances = [17, 18, 20, 30, 48, 66, 72, 80, 92, 110, 115, 138, 144, 168, 184, 207, 220]
+ilp_objective =    [11, 18, 11, 21, 47, 62, 49, 55, 64, 76, 106, 96, 100, 145, 122, 131, 154]
+ilp_time =         [0.72, 0.8, 2.57, 1.17, 5.72, 3.21, 23.89, 6.4, 6.82, 17.78, 4.58, 17.75, 16.34, 9.51, 57.83, 65.69, 52.08]
 
 
 def plot_grasp_vs_alpha(grasp_alpha, grasp_time, grasp_objective):
